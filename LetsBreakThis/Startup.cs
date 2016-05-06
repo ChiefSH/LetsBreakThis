0a32a9a1ce195b3870cd45819f9de6ef4c239deb
milesfw <https://github.com/ChiefSH>
2016-05-06 09:37:08 -0400

    Add project files.

---
+++ LetsBreakThis/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LetsBreakThis.Startup))]
+namespace LetsBreakThis
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
